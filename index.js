89252e538a5b2eafe06eeec1cdb87f7f7761bce6
Luis Ramos <luisramos@Luiss-MacBook-Pro.local>
2022-07-11 00:44:36 -0500

    MongoDB Configuration

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 const express = require("express");
+const mongoose = require("mongoose");
+require("dotenv").config();
 
 const app = express();
 
 app.use(express.json());
 
 app.use("/", require("./routes/stations"));
+
+mongoose
+  .connect(process.env.MONGODB)
+  .then(() => console.log("DB Connected..."))
+  .catch((error) => console.error(error));
 
 app.listen(3000, () => {
   console.log("SERVER RUNNING IN PORT: 3000");
